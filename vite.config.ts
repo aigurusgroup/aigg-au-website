b2abc615132648fcc38a22f40903bf114e842064
aigurusgroup <geouffrey@aigurusgroup.com>
2026-04-17 11:53:52 +0200

    Update vite.config.ts temp basepath before custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: basePath,
+  base: '/aigg-au-website/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
